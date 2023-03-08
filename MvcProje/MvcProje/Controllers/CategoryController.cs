aa6c2bf34b752adf77edf2e598c092ef139f3f04
akiffkocc <akiffkocc60@gmail.com>
2023-03-08 18:26:41 +0300

    MvcKamp
    Mvc Kamp

---
+++ MvcProje/MvcProje/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
